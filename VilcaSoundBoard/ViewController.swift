d9884989d61e3e419463b61af814e2623a3b765f
EduardoVilca <eduardo.vilca@tecsup.edu.pe>
2024-05-13 16:38:41 -0500

    Initial Commit

---
+++ VilcaSoundBoard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VilcaSoundBoard
+//
+//  Created by Eduardo Vilca on 13/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
